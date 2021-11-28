2fdcd349ea7845b11a167c480a15f6b5bb018a7c
Quickmotions <fergusbhaak@gmail.com>
2021-11-28 18:54:11 +0000

    New Codewars Mini Project, Justify lines of string. with specified width modififier.

---
+++ Text Justify/justify.py
@@ -0,0 +1,13 @@
+# Use spaces to fill in the gaps between words.
+# Each line should contain as many words as possible.
+# Use '\n' to separate lines.
+# Gap between words can't differ by more than one space.
+# Lines should end with a word not a space.
+# '\n' is not included in the length of a line.
+# Large gaps go first, then smaller ones ('Lorem--ipsum--dolor--sit-amet,' (2, 2, 2, 1 spaces)).
+# Last line should not be justified, use only one space between words.
+# Last line should not contain '\n'
+# Strings with one word do not need gaps ('somelongword\n').
+
+
+def justify(text, width):
