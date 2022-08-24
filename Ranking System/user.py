c80374530593a46865cd3e9e4c68a320ea994944
Quickmotions <fergusbhaak@gmail.com>
2022-08-24 11:04:54 +0100

    Ranking system prototype completed

---
+++ Ranking System/user.py
@@ -0,0 +1,57 @@
+# Codewars style ranking system - 24/08/2022 - Fergus Haak
+# https://www.codewars.com/kata/51fda2d95d6efda45e00004e/train/python
+
+class User:
+    def __init__(self):
+        self.rank = -8
+        self.progress = 0
+
+    def rank_minus_one(self):
+        if self.rank - 1 == 0:
+            return self.rank - 2
+        return self.rank - 1
+
+    def inc_progress(self, act_rank: int):
+        if act_rank == self.rank:
+            self.progress += 3
+        elif act_rank == self.rank_minus_one():
+            self.progress += 1
+        elif act_rank not in [-1, -2, -3, -4, -5, -6, -7, -8, 1, 2, 3, 4, 5, 6, 7, 8]:
+            raise Exception
+        elif act_rank > self.rank != 8:
+            if act_rank > 0 and self.rank < 0:
+                act_rank -= 1
+            self.progress += abs(self.rank - act_rank) * abs(self.rank - act_rank) * 10
+        while self.progress >= 100:
+            self.progress -= 100
+            self.rank += 1
+            if self.rank == 0: self.rank = 1
+        if self.rank >= 8:
+            self.rank = 8
+            self.progress = 0
+
+
+# tests
+user = User()
+print(user.rank)
+
+user.inc_progress(-2)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(-5)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(7)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(4)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(-1)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(7)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(7)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(-6)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(-4)
+print(f"rank: {user.rank} progress: {user.progress}")
+user.inc_progress(2)
+print(f"rank: {user.rank} progress: {user.progress}")
