c11f2c29b19e8de6276363d26cee419e93c1ecc6
Quickmotions <fergusbhaak@gmail.com>
2022-08-24 13:53:50 +0100

    created log in system

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+F
+FuzzySearch/fuzfind.py,7\4\74eecbe330467cb2e992442bf2c842d1a069d99f
+F
+Ranking System/user.py,c\0\c08f53af7f752f89338a716ee32cdd24321b75a1
+`
+0Catching Car Mileage Numbers/CarMileageFinder.py,d\b\db2b46f29eaf59978388d95619f8364627eb6bfc
+I
+BinaryTreeSort/BinTree.py,9\1\9193c6460ffef3b1926bf8708b47f58b96bd58f4
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+R
+"PickleStorage/DataStoragePickle.py,3\a\3a93d03007d103dada8e612950b2b2ac99629849
---
+++ Login System/login.py
@@ -0,0 +1,81 @@
+# 24/08/2022 - Fergus Haak - Login System
+import pickle
+
+
+class User:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+
+def load_users() -> dict[str:User]:
+    with open("data/users.pickle", "rb") as f:
+        return pickle.load(f)
+
+
+class UserManager:
+    def __init__(self):
+        self.users = load_users()
+
+    def new_user(self, name: str, password: str):
+        self.users[name] = User(name, password)
+
+    def save_users(self):
+        with open("data/users.pickle", "wb") as f:
+            pickle.dump(self.users, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+class Program:
+    def __init__(self):
+        self.current_menu = None
+        self.current_user: User = None
+        self.options = ["login", "register"]
+        self.user_manager = UserManager()
+
+    def start(self):
+        self.show_options()
+
+    def show_options(self):
+        self.current_menu = "main"
+        option_str = "OPTIONS:\n------------\n"
+        num = 1
+        for option in self.options:
+            option_str += f"{num} | {option}\n"
+            num += 1
+        choice = int(input(f"{option_str}\nChoice: "))
+        if self.options[choice-1] == "login":
+            self.login()
+        if self.options[choice-1] == "register":
+            self.register()
+
+    def register(self):
+        self.current_menu = "register"
+        print("-----------")
+        username = input("Create Username: ").lower()
+        password = input("Create Password: ")
+        self.user_manager.new_user(username, password)
+        print(f"\n\nCreated new user: {username.upper()}\n\n")
+        self.user_manager.save_users()
+        self.show_options()
+
+    def login(self):
+        self.current_menu = "login"
+        print("-----------")
+        username = input("Username: ").lower()
+        password = input("Password: ")
+        if username in self.user_manager.users:
+            if self.user_manager.users[username].password == password:
+                self.current_user = self.user_manager.users[username]
+                print(f"Logged in as {username.upper()}")
+                self.current_menu = "hub"
+                self.logged()
+                return
+        print("\n\nINCORRECT password or username\n\n")
+        self.show_options()
+
+    def logged(self):
+        self.current_menu = "logged in"
+
+
+program = Program()
+program.start()
